9be523aaaac7188a5f1fb0982e39cbbc9d09230b
Nick Woods <nicholas.andrew.woods@gmail.com>
2018-06-08 23:28:50 +0000

    Add Game#play

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,15 @@ class Game
   attr_accessor :game_state
   
   def initialize
-    self.game_state = [[nil] * 7] * 6
+    @game_state = Array.new(6) { Array.new(7) }
+  end
+  
+  def play(player, column)
+    i = 5
+    while !@game_state[i][column - 1].nil?
+      i -= 1
+      return false if i < 0
+    end
+    @game_state[i][column - 1] = player
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,4 +6,66 @@ describe Game do
       expect(Game.new.game_state).to eq([[nil] * 7] * 6)
     end
   end
+  
+  describe "#play" do
+    context "when given column is empty" do
+      subject do
+        game = Game.new
+        game.play(1, 4)
+        game.game_state
+      end
+      
+      it "adds a piece to the bottom row" do
+        expect(subject[5][3]).to eq(1)
+      end
+    end
+  
+  
+    context "when given column is neither empty nor full" do
+      subject do
+        game = Game.new
+        
+        game.play(1, 6)
+        game.play(2, 6)
+        
+        game.play(1, 3)
+        game.play(2, 3)
+        game.play(1, 3)
+        game.play(2, 3)
+        game.play(1, 3)
+        
+        game.game_state
+      end
+      
+      it "adds a piece to the correct row" do
+        expect(subject[4][5]).to eq(2)
+        expect(subject[1][2]).to eq(1)
+      end
+    end
+    
+    context "when given column is full" do
+      subject do
+        game = Game.new
+        game2 = Game.new
+        
+        i = 1
+        6.times do
+          game.play(i, 5)
+          i = (i + 1) % 2
+        end
+        
+        i = 1
+        7.times do
+          game2.play(i, 5)
+          i = (i + 1) % 2
+        end
+        
+        [game.game_state, game2.game_state]
+      end
+      
+      it "does not change game_state" do
+        expect(subject.first).to eq(subject.last)
+      end
+    end
+  end
 end
